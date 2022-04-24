77eb4fb95b24d45147fd9566afb57383257a6094
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-04-24 11:16:38 -0600

    Add documentation to shuffleArray function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Creates an array of length n with numbers 1..n, then shuffles and returns it.
+ * @param {number} n The length of the array.
+ * @returns {number[]} The shuffled array of integers.
+ */
 function shuffleArray(n) {
     nums = Array.from({length: n}, (value, index) => index);
 
