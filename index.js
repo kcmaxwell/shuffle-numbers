56b49df30cf0469e7a8eca3ec8163a3388ab37e6
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-04-24 11:28:09 -0600

    Finish shuffleArray, add randomInRange

--- index.js
+++ index.js
@@ -6,7 +6,25 @@
 function shuffleArray(n) {
     nums = Array.from({length: n}, (value, index) => index);
 
+    for (let i = 0; i < n - 2; i++) {
+        let j = randomInRange(i, n - 1);
+
+        let temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
     return nums;
+}
+
+/**
+ * Return a random number between min and max, inclusive.
+ * @param {number} min The start of the range.
+ * @param {number} max The end of the range.
+ * @returns A random number between min and max, inclusive.
+ */
+function randomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 console.dir(shuffleArray(10000), {'maxArrayLength': null});
